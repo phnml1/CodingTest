32269133dde367e20f498e51526bc0bb377cdd7f
lee juyoung <juyung0903@naver.com>
2024-02-27 11:42:32 +0900

    24337가희와탑

---
+++ 코테문제집/24337가희와탑.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline;
+n,a,b = map(int,input().split());
+answer = []
+
+for i in range(1,a):
+  answer.append(i);
+answer.append(max(a,b));
+for i in range(b-1,0,-1):
+  answer.append(i);
+if len(answer)>n:
+  print(-1)
+else:
+  print(answer[0], end = ' ');
+  for i in range(n-len(answer)):
+    print(1,end=' ');
+  for i in range(1,len(answer)):
+    print(answer[i],end = ' ');
