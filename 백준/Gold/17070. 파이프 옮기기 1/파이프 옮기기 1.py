bad14ecf771839b6e90a8fe3940b3426425a2059
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-04-09 20:53:53 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -5,33 +5,17 @@ arr = []
 result = 0;
 for _ in range(n):
   arr.append(list(map(int,input().split())));
-def dfs(x,y,state):
-  global result;
-  if x==n-1 and y==n-1:
-    result += 1;
-  if state == 0:
-    if 0<=x<n and 0<=y+1<n:
-      if arr[x][y+1] == 0:
-        dfs(x,y+1,0)
-    if 0<=x+1<n and 0<=y<n and 0<=x<n and 0<=y+1<n and 0<=x+1<n and 0<=y+1<n:
-      if arr[x][y+1] == 0 and arr[x+1][y] == 0 and arr[x+1][y+1] ==0:
-        dfs(x+1,y+1,2);
-  if state == 1:
-    if 0<=x+1<n and 0<=y<n:
-      if arr[x+1][y] == 0:
-        dfs(x+1,y,1);
-    if 0<=x+1<n and 0<=y<n and 0<=x<n and 0<=y+1<n and 0<=x+1<n and 0<=y+1<n:
-      if arr[x][y+1] == 0 and arr[x+1][y] == 0 and arr[x+1][y+1] ==0:
-        dfs(x+1,y+1,2);
-  if state == 2:
-    if 0<=x+1<n and 0<=y<n:
-      if arr[x+1][y] == 0:
-        dfs(x+1,y,1);
-    if 0<=x<n and 0<=y+1<n:
-      if arr[x][y+1] == 0:
-        dfs(x,y+1,0)  
-    if 0<=x+1<n and 0<=y<n and 0<=x<n and 0<=y+1<n and 0<=x+1<n and 0<=y+1<n:
-      if arr[x][y+1] == 0 and arr[x+1][y] == 0 and arr[x+1][y+1] ==0:
-        dfs(x+1,y+1,2);
-dfs(0,1,0);
-print(result);
+dp = [[[0 for _ in range(n+1)] for _ in range(n+1)] for _ in range(3)];
+dp[0][0][1] = 1;
+
+for i in range(2,n):
+  if arr[0][i] == 0:
+    dp[0][0][i] = dp[0][0][i-1];
+for r in range(1,n):
+  for c in range(1,n):
+    if arr[r][c] == 0 and arr[r-1][c] == 0 and arr[r][c-1] == 0:
+      dp[2][r][c] = sum(dp[i][r-1][c-1] for i in range(3));
+    if arr[r][c] == 0:
+      dp[0][r][c] = dp[2][r][c-1] + dp[0][r][c-1];
+      dp[1][r][c] = dp[1][r-1][c] + dp[2][r-1][c];
+print(sum(dp[i][n-1][n-1] for i in range(3)));
