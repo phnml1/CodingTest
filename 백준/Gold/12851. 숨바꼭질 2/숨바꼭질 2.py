19fe9cd42e5a1de85fbf44ce2abf919c844d34b6
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-04-10 21:18:18 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 788 ms, Memory: 37632 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,29 +1,32 @@
-from collections import defaultdict
-import sys;
-import heapq;
-input = sys.stdin.readline;
-n,k = map(int,input().split());
-case = 0;
-visited = defaultdict(lambda: 1e9);
-def bfs(visited):
-  global case;
-  visited[n] = 0;
-  queue = []
-  heapq.heappush(queue,(0,n));
-  while queue:
-    cost,num = heapq.heappop(queue);
-    if cost < visited[num]:
-      visited[num] = cost;
-    if num == k:
-      if cost < visited[k]:
-        case = 1;
-      if cost == visited[k]:
-        case += 1;
-    if cost<=visited[num] and 0<=num<k:
-      heapq.heappush(queue,(cost+1,num+1));
-      heapq.heappush(queue,(cost+1,num*2));
-    if cost<=visited[num] and ((0<=num<k) or (k<num and num - k + cost<=visited[k])):
-      heapq.heappush(queue,(cost+1,num-1));
-bfs(visited);
-print(visited[k]);
-print(case);
+from sys import stdin
+input = stdin.readline
+from collections import deque
+
+# 수빈 위치, 동생 위치
+N, K = map(int,input().split())
+
+MAX_SIZE = 100001
+
+que = deque()
+que.append(N)
+visited = [-1]*MAX_SIZE
+visited[N] = 0
+cnt = 0
+
+while que:
+    # 현 위치
+    current = que.popleft()
+    # 도착
+    if current == K:
+        cnt += 1
+    # 이동
+    for next in [current*2,current+1,current-1]:
+        # 범위 내
+        if 0 <= next < MAX_SIZE:
+            # 첫방문 혹은 방문 시간이 같은 경우가 이미 있음(가장 빠른 시간 방법의 수를 위해)
+            if visited[next] == -1 or visited[next] >= visited[current] + 1:
+                visited[next] = visited[current] + 1
+                que.append(next)
+ 
+print(visited[K])
+print(cnt)
