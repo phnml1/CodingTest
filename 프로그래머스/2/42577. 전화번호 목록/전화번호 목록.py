65320e6a39f41a932fca90936b9a824afa4b9eb0
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-05-15 18:12:30 +0900

    [level 2] Title: 전화번호 목록, Time: 180.39 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,12 @@
 def solution(phone_book):
-    answer = True;
-    phone_book.sort();
-    for i in range(len(phone_book)-1):
-        result = False;
-        for j in range(len(phone_book[i])):
-            if phone_book[i][j] != phone_book[i+1][j]:
-                result = True;
-                break;
-        if result == False:
-            answer=False;
-            break;
+    hash_map = {};
+    for number in phone_book:
+        hash_map[number] = 1;
+    for number in phone_book:
+        temp = ''
+        for i in number:
+            temp += i;
+            if temp in hash_map and temp != number:
+                return False;
         
-    return answer
+    return True
