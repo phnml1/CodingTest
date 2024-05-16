1e0ae9a35ed16c9d3fab79020ce488560654bfbc
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-05-16 17:26:00 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 45.36 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,39 +1,24 @@
-from collections import deque
 def solution(land):
-    answer = 0
-    queue = deque();
-    d = [(1,0),(0,1),(-1,0),(0,-1)];
-    dic = {}            
-    def bfs(land,i,j,num):
-        count = 0;   
-        queue.append((i,j));
-        land[i][j] = num;
-        while queue:
-            r,c = queue.popleft();
-            count += 1;
-            for n in range(4):
-                dx,dy = d[n];
-                next_r, next_c = r+dx,c+dy;
-                if 0<=next_r<len(land) and 0<=next_c<len(land[0]):
-                    if land[next_r][next_c] == 1:
-                        land[next_r][next_c] = num;
-                        queue.append((next_r,next_c));
-        return count;
-    num = 2;
-    for i in range(len(land)): 
-        for j in range(len(land[0])):     
-            if land[i][j] == 1 and land[i][j] == 1:
-                dic[num] = bfs(land,i,j,num);
-                num+=1;
-    for i in range(len(land[0])):
-      visit = [];
-      result = 0;
-      for j in range(len(land)): 
-        if land[j][i] != 0 and land[j][i] not in visit:
-          result += dic[land[j][i]];
-          visit.append(land[j][i])
-      answer = max(result,answer);
-                                
-    return answer
-  
-print(solution(	[[0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]]))
+    n, m = len(land), len(land[0])
+    visited = [[True]*m for _ in range(n)]
+    delta = [(1,0),(-1,0),(0,1),(0,-1)]
+    oil_cnt = [0]*m
+    for i in range(n):
+        for j in range(m):
+            if land[i][j] and visited[i][j]:
+                visited[i][j] = False
+                s = [(i,j)]
+                col = set()
+                oil = 0
+                while s:
+                    x, y = s.pop()
+                    col.add(y)
+                    oil += 1
+                    for dx, dy in delta:
+                        X, Y = x+dx, y+dy
+                        if 0<=X<n and 0<=Y<m and land[X][Y] and visited[X][Y]:
+                            visited[X][Y] = False
+                            s.append((X,Y))
+                for y in col:
+                    oil_cnt[y] += oil
+    return max(oil_cnt)
