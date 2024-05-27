c78d4d75b0af361e2a271660be997b5dad21f640
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-05-27 13:35:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,35 +1,19 @@
-from collections import deque
-import copy;
+def dfs(v, graph, visited):
+    visited[v] = True
+    return sum([1] + [dfs(u, graph, visited) for u in graph[v] if not visited[u]])
+
 def solution(n, wires):
-    answer = 1e9
-    def bfs(start,visited, graph):
-        visited[start] = True;
-        count = 0;
-        queue = deque();
-        queue.append(start);
-        while queue:
-            cur_node = queue.popleft();
-            count += 1;
-            for next_node in graph[cur_node]:
-                if visited[next_node] == False:
-                    visited[next_node] = True;
-                    queue.append(next_node);
-                    
-        return count;
-    for w in wires:
-        temp = copy.deepcopy(wires);
-        temp.remove(w);
-        graph = [[] for _ in range(n+1)];
-        visited = [False for _ in range(n+1)];
-        results = [];
-        for wire in temp:
-            graph[wire[0]].append(wire[1]);
-            graph[wire[1]].append(wire[0]);
-        
-        for i in range(1,n+1):
-            if visited[i] == False:
-                results.append(bfs(i,visited,graph));
-        results.sort(reverse = True);
-        answer = min(answer,results[0] - results[1]);
-              
+    graph = [[] for _ in range(n+1)]
+    for v, u in wires:
+        graph[v].append(u)
+        graph[u].append(v)
+
+    answer = 100
+    for i in range(n-1):
+        visited = [False for _ in range(n+1)]
+        v1, v2 = wires[i]
+        visited[v2] = True
+        tmp = abs(dfs(v1, graph, visited) - dfs(v2, graph, visited))
+        answer = min(tmp, answer)
+
     return answer
