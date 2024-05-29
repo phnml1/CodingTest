75c9a690830beeaff56a023dba11efe64bb5ed3a
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-05-29 16:09:16 +0900

    [level 2] Title: 구명보트, Time: 7.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,15 +2,12 @@ def solution(people, limit):
     people.sort();
     start, end = 0, len(people) - 1;
     answer, passengers = 0,0;
-    
     while start < end:
         cur_weight = people[start] + people[end];
         if cur_weight<=limit:
             passengers += 2;
             answer += 1;
             start += 1;
-            end -= 1;
-        else:
-            end -= 1;
+        end-=1;
     answer += (len(people) - passengers);
     return answer
