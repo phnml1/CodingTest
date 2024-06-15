2bb6e9b73d429b7b6fc28eb18e2478690f8a2e6f
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-06-15 20:55:20 +0900

    [level 3] Title: 정수 삼각형, Time: 49.57 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -5,7 +5,7 @@ def solution(triangle):
             if j==0:
                 triangle[i][j] += triangle[i-1][j];
             elif j == len(triangle[i])-1:
-                triangle[i][j] += triangle[i-1][len(triangle[i-1])-1];
+                triangle[i][j] += triangle[i-1][-1];
             else:
                 triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j]);
-    return max(triangle[len(triangle)-1]);
+    return max(triangle[-1]);
