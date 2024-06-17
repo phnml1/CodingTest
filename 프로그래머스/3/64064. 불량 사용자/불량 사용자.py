beed49033522439051b94e666159cb7df6d8722b
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-06-17 16:12:06 +0900

    [level 3] Title: 불량 사용자, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,26 +1,67 @@
-from itertools import permutations;
-from collections import defaultdict
-answer = 0
-def compare(user_word,ban_word):
-    if len(user_word) != len(ban_word):
-        return False;
-    
-    for i in range(len(user_word)):
-        if user_word[i] != ban_word[i]:
-            if ban_word[i] != '*':
-                return False;
-    return True;
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+def combi(temp, number, calculate):
+    global result
+    if len(temp) == len(calculate):
+        temp = set(temp)
+        if temp not in result:
+            result.append(temp)
+        return
+    else:
+        for j in range(len(calculate[number])):
+            if calculate[number][j] not in temp:
+                temp.append(calculate[number][j])
+                combi(temp, number+1, calculate)
+                temp.pop()
+result = []
 def solution(user_id, banned_id):
-    ban_set = []
-    for case in list(permutations(user_id, len(banned_id))):
-        case = list(case);
-        flag = True;
-        for i in range(len(case)):
-            if compare(case[i],banned_id[i]) == False:
-                flag = False;
-                break;
-        if flag:
-            case = set(case);
-            if case not in ban_set:
-                ban_set.append(case);
-    return len(ban_set)
+    global result
+    calculate = []
+    for ban in banned_id:
+        possible=[]
+        for user in user_id:
+            if len(ban) != len(user):
+                continue
+            else:
+                count = 0
+                for i in range(len(ban)):
+                    if user[i] == ban[i]:
+                        count+=1
+                if count == len(ban)-ban.count('*'):
+                    possible.append(user)
+        calculate.append(possible)
+
+    combi([], 0, calculate)
+    return len(result)
