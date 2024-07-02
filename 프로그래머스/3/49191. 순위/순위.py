d0adcf7dc5e748d391ad54275b88eaab7c900cbb
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-07-02 18:57:05 +0900

    [level 3] Title: 순위, Time: 162.88 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,32 +1,17 @@
-from collections import defaultdict, deque;
-
-def bfs(win_nodes, lose_nodes,a,visited):
-    queue = deque();
-    queue.append(a);
-    while queue:
-        node = queue.popleft();
-        for i in win_nodes[node]:
-            if visited[i] == False:
-                queue.append(i);
-                visited[i] = True;
-    queue.append(a);
-    while queue:
-        node = queue.popleft();
-        for i in lose_nodes[node]:
-            if visited[i] == False:
-                queue.append(i);
-                visited[i] = True;
-    return visited.count(True);
 def solution(n, results):
     answer = 0
-    win_nodes = defaultdict(list);
-    lose_nodes = defaultdict(list);    
+    floyd = [[0 for _ in range(n+1)] for _ in range(n+1)]
     for result in results:
-        a,b = result;
-        lose_nodes[b].append(a);
-        win_nodes[a].append(b);
+        floyd[result[0]][result[1]] = 1;
+        floyd[result[1]][result[0]] = -1;
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if floyd[i][k] == 1 and floyd[k][j] == 1:
+                    floyd[i][j] = 1;
+                if floyd[i][k] == -1 and floyd[k][j] == -1:
+                    floyd[i][j] = -1;
     for i in range(1,n+1):
-        visited = [False for _ in range(n+1)];
-        if bfs(win_nodes, lose_nodes,i,visited) == n-1:
+        if floyd[i].count(1) + floyd[i].count(-1) == n-1:
             answer += 1;
     return answer
