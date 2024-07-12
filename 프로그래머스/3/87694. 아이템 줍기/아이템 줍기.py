3de0880d258842f35e2399a085d7ea52f71d3f2c
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-07-12 15:18:47 +0900

    [level 3] Title: 아이템 줍기, Time: 1.24 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -2,26 +2,26 @@ from collections import deque
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
     board = [[-1 for _ in range(102)] for _ in range(102)];
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dir = [(1,0),(0,1),(-1,0),(0,-1)];
     for r in rectangle:
-        x1,y1,x2,y2 = map(lambda x: x*2, r);
+        x1,y1,x2,y2 = map(lambda x: x*2,r);
         for i in range(x1,x2+1):
             for j in range(y1,y2+1):
                 if x1<i<x2 and y1<j<y2:
                     board[i][j] = 0;
                 elif board[i][j] != 0:
                     board[i][j] = 1;
-    queue=deque();
-    queue.append((characterX*2, characterY*2));
-    visited = [[1 for _ in range(102)] for _ in range(102)];
+                    print((i,j));
+    visited = [[0 for _ in range(102)] for _ in range(102)];
+    queue = deque();
+    queue.append((characterX*2,characterY*2));
+    visited[characterX*2][characterY*2] = 1;
     while queue:
         x,y = queue.popleft();
-        if x == itemX*2 and y == itemY*2:
-            return visited[x][y]//2
-        for i in range(4):
-            next_x, next_y = x + dx[i], y + dy[i];
-            if 0<=next_x<102 and 0<=next_y<102:
-                if board[next_x][next_y] == 1 and visited[next_x][next_y] == 1:
-                    queue.append((next_x,next_y));
-                    visited[next_x][next_y] = visited[x][y] + 1;
+        for dx,dy in dir:
+            nx,ny = x+dx,y+dy;
+            if 0<=nx<102 and 0<=ny<102:
+                if board[nx][ny] == 1 and visited[nx][ny] == 0:
+                    queue.append((nx,ny));
+                    visited[nx][ny] = visited[x][y] + 1;
+    return visited[itemX*2][itemY*2]//2 
