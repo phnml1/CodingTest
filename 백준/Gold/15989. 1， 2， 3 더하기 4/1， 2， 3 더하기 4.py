63e40c46c732285eb53d55333bd79915985cdf58
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-08-19 15:27:02 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,15 +1,20 @@
-import sys; 
+#다시풀어라
+import sys;
 input = sys.stdin.readline;
 
-t = int(input());
-arr = []
-for _ in range(t):
-  arr.append(int(input()));
+dp = [[0 for _ in range(10001)] for _ in range(4)];
+dp[1][0],dp[2][0],dp[3][0] = 1,1,1;
 
-DP = [0 for _ in range(10001)];
-DP[0] = 1;
-for i in range(1,4):
-  for j in range(i,10001):
-    DP[j] = DP[j-i]+DP[j];
-for num in arr:
-  print(DP[num])
+for i in range(1,10001):
+  dp[1][i] = 1;
+for i in range(2,4):
+  for j in range(1,10001):
+    if j - i>=0:
+      dp[i][j] = dp[i][j-i] + dp[i-1][j];
+    else:
+      dp[i][j] = dp[i-1][j];
+t = int(input());
+for _ in range(t):
+  print(dp[3][int(input())]);
+
+
