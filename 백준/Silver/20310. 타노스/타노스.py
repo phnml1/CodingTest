32486e9b76feffaccc8d2978a1c46a094695c2f2
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-08-21 15:32:39 +0900

    [Silver III] Title: 타노스, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -7,7 +7,9 @@ result_index = [];
 result = '';
 for i in range(len(s)):
   index[int(s[i])].append(i);
-result_index = index[0][:len(index[0])//2] + index[1][len(index[1])//2:]
+result_index = index[0][:len(index[0])//2] + index[1][len(index[1])//2:];
+result_index.sort();
 for i in result_index:
   result += s[i];
 print(result); 
+    
