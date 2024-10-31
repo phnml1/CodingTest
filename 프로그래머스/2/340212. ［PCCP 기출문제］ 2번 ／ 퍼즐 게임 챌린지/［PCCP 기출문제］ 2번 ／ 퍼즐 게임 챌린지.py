c9dc9574f4b8610a510274b527fc00b88b9cf6f3
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-10-31 12:11:35 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 2349.36 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,13 +1,13 @@
 def solution(diffs, times, limit):
-    max_diff = max(diffs)
+    max_level = max(diffs)
     l = 1
-    r = max_diff;
-    answer = max_diff;
+    r = max_level;
+    answer = max_level;
     while l<r:
         level = (l+r)//2;
         time = times[0]
         for i in range(1,len(diffs)):
-            w_count = diffs[i]-level;
+            w_count = diffs[i] - level;
             if w_count > 0:
                 time += w_count*(times[i-1] + times[i]) + times[i];
             else:
