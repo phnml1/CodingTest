8bdb1267dfc64d15facf482540dbc9f95e13fb9d
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-11-05 00:07:22 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 604.92 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 3번 / 충돌위험 찾기 - 340211 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211#qna) 
 
 ### 성능 요약
 
-메모리: 176 MB, 시간: 638.34 ms
+메모리: 176 MB, 시간: 604.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 18:29:23
+2024년 11월 05일 00:07:17
 
 ### 문제 설명
 
--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,43 +1,36 @@
-from collections import Counter
+from collections import Counter;
 
-
+def move(graph, points, route):
+    time = 0
+    for i in range(len(route)-1):
+        startr,startc,endr,endc = points[route[i]-1][0],points[route[i]-1][1],points[route[i+1]-1][0],points[route[i+1]-1][1];
+        while startr != endr:
+            graph.append((time,startr,startc));
+            if startr<endr:
+                startr += 1;
+            else:
+                startr -= 1;
+            time += 1;
+        while startc != endc:
+            graph.append((time,startr,startc));
+            if startc<endc:
+                startc += 1;
+            else:
+                startc -= 1;
+            time += 1;
+    graph.append((time,startr,startc));
+            
+                
+            
 def solution(points, routes):
-    def bfs(route):
-        idx = 0
-        pa = []
-        for i in range(len(route) - 1):
-            sx, sy = spots[route[i] - 1]
-            ex, ey = spots[route[i + 1] - 1]
-
-            # x 좌표 맞추기
-            while sx != ex:
-                pa.append((sx, sy, idx))
-                if sx < ex:
-                    sx += 1
-                else:
-                    sx -= 1
-                idx += 1
-
-            # y 좌표 맞추기
-            while sy != ey:
-                pa.append((sx, sy, idx))
-                if sy < ey:
-                    sy += 1
-                else:
-                    sy -= 1
-                idx += 1
-        pa.append((sx, sy, idx))
-        return pa
-
-    spots = [i for i in points]
-    second = []
-
+    answer = 0;
+    graph = [];
     for route in routes:
-        second.extend(bfs(route))
-    res = 0
-    temp = Counter(second)
+        move(graph,points,route);
+    temp = Counter(graph);
     for i in temp.values():
-        if i >= 2:
-            res += 1
-
-    return res
+        if i>=2:
+            answer += 1;
+    return answer;
+                
+        
