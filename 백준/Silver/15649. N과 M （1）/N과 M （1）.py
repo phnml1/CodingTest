8011c8e2f60d6b96bfd95245f1506e682b8ca408
lee ju young <88328887+phnml1@users.noreply.github.com>
2024-12-12 11:46:04 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,12 +1,11 @@
-import copy
 n,m = map(int,input().split());
-def dfs(arr,index):
-    if index == m:
-        print(' '.join(arr));
-        return;
-    for i in range(1,n+1):
-        new_arr = copy.deepcopy(arr);
-        if str(i) not in new_arr:
-            new_arr.append(str(i));
-            dfs(new_arr,index+1);
-dfs([],0);
+def dfs(arr):
+   if len(arr) == m:
+        print(*arr);
+        return
+   for i in range(1,n+1):
+        if i not in arr:
+            arr.append(i);
+            dfs(arr);
+            arr.pop();
+dfs([]);
