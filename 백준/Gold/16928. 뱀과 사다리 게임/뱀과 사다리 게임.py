f053c960c939d2ed97a27ef6302f59537d5c67f1
lee ju young <88328887+phnml1@users.noreply.github.com>
2025-03-29 16:08:01 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 56 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,30 +1,27 @@
-import sys
 from collections import defaultdict, deque
+import sys
 input = sys.stdin.readline;
+
 n,m = map(int,input().split());
-result = 0;
-costs = [1e9 for _ in range(101)];
-path = defaultdict(int);
-dir = [1,2,3,4,5,6];
-for _ in range(n):
-  s,t = map(int,input().split());
-  path[s]=t;
-for _ in range(m):
-  s,t = map(int,input().split());
-  path[s]=t;
-start = 1;
+dict = defaultdict(int);
+a = [1e9 for _ in range(101)];
+for _ in range(n+m):
+    x,y = map(int,input().split());
+    dict[x] = y;
 queue = deque();
-queue.append((0,start));
+a[1] = 0;
+queue.append((0,1));
 while queue:
-  cur_cost, cur_node = queue.popleft();
-  for i in range(6):
-    next_node = cur_node + dir[i];
-    next_cost = cur_cost + 1;
-    if 1> next_node or next_node> 100:
-      continue;
-    if path[next_node]!=0:
-      next_node = path[next_node];
-    if next_cost < costs[next_node]:
-      costs[next_node] = next_cost;
-      queue.append((next_cost,next_node));
-print(costs[100]);
+    count, can = queue.popleft();
+    if can == 100:
+        break;
+    for i in range(1,7):
+        next_count = count + 1;
+        next_can = can + i
+        if 0<next_can <= 100:
+            if next_can in dict:
+                next_can = dict[next_can];
+            if next_count < a[next_can]:
+                a[next_can] = next_count;
+                queue.append((next_count,next_can));
+print(a[100]);
