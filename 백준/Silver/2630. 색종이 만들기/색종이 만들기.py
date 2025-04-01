3ef8fa93dc3f8aafd766b46ec6a94a30ad363f5d
lee ju young <88328887+phnml1@users.noreply.github.com>
2025-04-01 13:38:38 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,25 +1,28 @@
+import sys
+input = sys.stdin.readline;
+
 n = int(input());
-arr = []
-ans_0,ans_1 = 0,0;
-for i in range(n):
+arr = [];
+for _ in range(n):
   arr.append(list(map(int,input().split())));
-def dfs(x,y,n):
-  global ans_0,ans_1;
-  color = arr[x][y];
-  for i in range(x,x+n):
-    for j in range(y,y+n):
-      if color != arr[i][j]:
-        dfs(x,y,n//2);
-        dfs(x,y+n//2,n//2);
-        dfs(x+n//2,y,n//2);
-        dfs(x+n//2,y+n//2,n//2);
-        return;
-  if color == 0:
-    ans_0 += 1;
-  if color == 1:
-    ans_1 += 1;
-  
-  return;
-dfs(0,0,n);
-print(ans_0);
-print(ans_1);
+result = [0,0];
+def recur(x,y,num):
+  prev = arr[x][y];
+  flag = True;
+  if num == 1:
+    result[prev] += 1;
+    return;
+  for i in range(x,x+num):
+    for j in range(y,y+num):
+      if prev != arr[i][j]:
+        flag = False;
+        break;
+  if flag:
+    result[prev] += 1;
+  else:
+    for i in range(2):
+      for j in range(2):
+        recur(x+num//2*i,y+num//2*j,num//2);
+recur(0,0,n);
+for i in range(2):
+  print(result[i]);
