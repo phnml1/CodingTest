1b587a049337ee86716e0543d4402cfc645f072e
lee ju young <88328887+phnml1@users.noreply.github.com>
2025-04-04 18:47:46 +0900

    [Silver III] Title: N과 M (3), Time: 624 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline;
+
+n,m = map(int,input().split());
+
+def backtracking(arr,i,r):
+  if r==m:
+    print(' '.join(arr));
+    return;
+  else:
+    for a in range(1,n+1):
+      arr.append(str(a));
+      backtracking(arr,a,r+1);
+      arr.pop();
+for i in range(1,n+1):
+  backtracking([str(i)],i,1);
